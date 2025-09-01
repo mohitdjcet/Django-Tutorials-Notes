95c35d3f7f66fa15b52fec9a27f3ddeb6c375612
Mohit Kumar <mks.djcet@gmail.com>
2025-09-01 14:27:31 +0530

    Updated code

---
+++ myProject13/blog/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    enrollment_date = models.DateField(auto_now_add=True)
