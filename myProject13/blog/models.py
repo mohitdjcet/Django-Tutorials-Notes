6488ac58afc638e10b7655ea6d16de8bfb100d39
Mohit Kumar <mks.djcet@gmail.com>
2025-09-03 21:32:59 +0530

    Updated code

--- myProject13/blog/models.py
+++ myProject13/blog/models.py
@@ -4,3 +4,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(unique=True)
+    city = models.CharField(max_length=100,default='Unknown')
+
+    def __str__(self):
+        return self.name
